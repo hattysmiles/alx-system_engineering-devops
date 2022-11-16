2842cd56e55da30752a083b83f422269ff4a3eba
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-11-16 22:36:07 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:)[\w|\+|\-|\:]*(?=\])/).join(",")
+res = ARGV[0].scan(/\[from:(.+?)\]|\[to:(.+?)\]|\[flags:(.+?)\]/)
+list = [res[0].compact, res[1].compact, res[2].compact]
+puts list.join(',')
